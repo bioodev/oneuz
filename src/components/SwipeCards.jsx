036fbf3fb199828d796467a3099639511cbb04d0
bioodev <bioodev@protonmail.com>
2023-03-15 21:27:46 -0300

    upload

---
+++ src/components/SwipeCards.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Navigation } from "swiper";
+
+const SwiperCards = () => {
+  return (
+    <div>
+      <Swiper
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        centeredSlides={true}
+        loop={true}
+        navigation={true}
+        modules={[Autoplay, Navigation]}
+        slidesPerView={1}
+        spaceBetween={10}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="flex flex-col gap-2">
+            <img width="600" src="images/1.jpg" alt="" />
+            <h2 className="px-2">Titulo #1</h2>
+            <p className="px-2">descripcion</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col gap-2">
+            <img width="600" src="images/2.jpg" alt="" />
+            <h2 className="px-2">Titulo #1</h2>
+            <p className="px-2">descripcion</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col gap-2">
+            <img width="600" src="images/3.jpg" alt="" />
+            <h2 className="px-2">Titulo #1</h2>
+            <p className="px-2">descripcion</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col gap-2">
+            <img width="600" src="images/4.jpg" alt="" />
+            <h2 className="px-2">Titulo #1</h2>
+            <p className="px-2">descripcion</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col gap-2">
+            <img width="600" src="images/5.jpg" alt="" />
+            <h2 className="px-2">Titulo #1</h2>
+            <p className="px-2">descripcion</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col gap-2">
+            <img width="600" src="images/6.jpg" alt="" />
+            <h2 className="px-2">Titulo #1</h2>
+            <p className="px-2">descripcion</p>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="flex flex-col gap-2">
+            <img width="600" src="images/8.jpg" alt="" />
+            <h2 className="px-2">Titulo #1</h2>
+            <p className="px-2">descripcion</p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default SwiperCards;
